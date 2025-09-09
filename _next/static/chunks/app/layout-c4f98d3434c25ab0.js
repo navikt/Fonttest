7d51243dd98ba8ee7ba8d9b99e870b1fba8c3670
Linda Lyngstad <lindalyngstad@Lindas-MacBook-Pro-4.local>
2025-09-09 11:36:38 +0200

    Updates

---
+++ _next/static/chunks/app/layout-c4f98d3434c25ab0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6658:function(n,e,t){Promise.resolve().then(t.t.bind(t,5780,23)),Promise.resolve().then(t.t.bind(t,7960,23))},7960:function(){},5780:function(){}},function(n){n.O(0,[389,587,971,117,744],function(){return n(n.s=6658)}),_N_E=n.O()}]);
